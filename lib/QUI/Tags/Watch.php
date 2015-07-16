41be4042e68b09e9e7956a7807bb472a3ff01b55
Henning Leutz <leutz@pcsg.de>
2015-07-16 15:49:38 +0200

    watch events

---
+++ lib/QUI/Tags/Watch.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file contains \QUI\Tags\Watch
+ */
+
+namespace QUI\Tags;
+
+use QUI;
+
+/**
+ * Class Watch
+ *
+ * @package quiqqer/tags
+ */
+class Watch
+{
+    /**
+     *
+     * @param string $call
+     * @param array $params
+     * @param array $result
+     *
+     * @return string
+     */
+    static function watchText($call, $params, $result)
+    {
+        switch ($call) {
+
+            case 'package_quiqqer_tags_ajax_tag_add':
+                return QUI::getLocale()->get('quiqqer/tags', 'watch.add.tags', array(
+                    'tag' => $params['tag']
+                ));
+
+            case 'package_quiqqer_tags_ajax_tag_delete':
+
+                $tags = json_decode($params['tags'], true);
+
+                return QUI::getLocale()->get('quiqqer/tags', 'watch.delete.tags', array(
+                    'tag' => implode(',', $tags)
+                ));
+
+            case 'package_quiqqer_tags_ajax_tag_edit':
+                return QUI::getLocale()->get('quiqqer/tags', 'watch.edit.tags', array(
+                    'tag' => $params['tag']
+                ));
+        }
+
+        return '####';
+    }
+}
