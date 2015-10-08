8c2efbb2d9daa06338abbd3dac914f4fb395d48d
Henning Leutz <leutz@pcsg.de>
2015-10-08 14:40:04 +0200

    Fix wegen Site->getUrlRwrited() patch $_REQUEST['_url'] hat kein / am anfang

--- types/tag-listing.php
+++ types/tag-listing.php
@@ -6,30 +6,30 @@ $url = pathinfo($url);
 $siteUrl = $Site->getUrlRewrited();
 
 // tag
-if ($siteUrl != $_REQUEST['_url']
-    && $siteUrl == $url['dirname'].\QUI\Rewrite::URL_DEFAULT_SUFFIX
+if ($siteUrl != '/' . $_REQUEST['_url']
+    && $siteUrl == '/' . $url['dirname'] . \QUI\Rewrite::URL_DEFAULT_SUFFIX
 ) {
 
     try {
 
-        $title = $Site->getAttribute('title');
+        $title   = $Site->getAttribute('title');
         $Manager = new \QUI\Tags\Manager($Project);
-        $tag = $Manager->get($url['filename']);
+        $tag     = $Manager->get($url['filename']);
 
         if (isset($tag['image']) && !empty($tag['image'])) {
             $Site->setAttribute('image_emotion', $tag['image']);
         }
 
         if (isset($tag['title'])) {
-            $Site->setAttribute('meta.seotitle', $title.' - '.$tag['title']);
+            $Site->setAttribute('meta.seotitle', $title . ' - ' . $tag['title']);
         } else {
-            $Site->setAttribute('meta.seotitle', $title.' - '.$tag['tag']);
+            $Site->setAttribute('meta.seotitle', $title . ' - ' . $tag['tag']);
         }
 
         $TagSite = new \QUI\Projects\Site\Virtual(array(
             'id'    => $Site->getId(),
             'name'  => $tag['tag'],
-            'url'   => URL_DIR.$_REQUEST['_url'],
+            'url'   => URL_DIR . $_REQUEST['_url'],
             'title' => $tag['title']
         ), $Project, $Site);
 
@@ -38,7 +38,7 @@ if ($siteUrl != $_REQUEST['_url']
         $Engine->assign(array(
             'tag'   => $tag,
             'sites' => $Manager->getSitesFromTags(array($tag['tag']))
-    ));
+        ));
 
     } catch (QUI\Exception $Exception) {
 
