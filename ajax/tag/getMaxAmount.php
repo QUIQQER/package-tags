fbfd85382bf027f0ea258b2202424c29478b74fc
Jan Wennrich <jan@pcsg.de>
2018-07-12 11:51:49 +0200

    feat: disable input if user can't add more tags (closes #51)

---
+++ ajax/tag/getMaxAmount.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file contains package_quiqqer_tags_ajax_tag_getMaxAmount
+ */
+
+/**
+ * Returns the maximum amount of tags a user can add
+ *
+ * @return int
+ */
+QUI::$Ajax->registerFunction(
+    'package_quiqqer_tags_ajax_tag_getMaxAmount',
+    function () {
+        return QUI::getUserBySession()->getPermission('tags.siteLimit', 'maxInteger');
+    },
+    array()
+);
