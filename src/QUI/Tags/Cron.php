54a6e99990025d82d0196871af350ed08c8c1739
addius8 <addius8@web.de>
2020-09-24 10:12:59 +0200

    feat: groups were added to the tag_Site_Cache

--- src/QUI/Tags/Cron.php
+++ src/QUI/Tags/Cron.php
@@ -7,6 +7,7 @@
 namespace QUI\Tags;
 
 use QUI;
+use function DusanKasan\Knapsack\isEmpty;
 
 /**
  * Tag Crons - Crons for the tag system
@@ -31,14 +32,14 @@ class Cron
             return;
         }
 
-
-        $Project  = QUI::getProject($params['project'], $params['lang']);
+        $Project = QUI::getProject($params['project'], $params['lang']);
         $DataBase = QUI::getDataBase();
 
-        $tableSites     = QUI::getDBProjectTableName('tags_sites', $Project);
+        $tableSites = QUI::getDBProjectTableName('tags_sites', $Project);
         $tableSiteCache = QUI::getDBProjectTableName('tags_siteCache', $Project);
-        $tableCache     = QUI::getDBProjectTableName('tags_cache', $Project);
+        $tableCache = QUI::getDBProjectTableName('tags_cache', $Project);
 
+        $tableTagGroups = QUI::getDBProjectTableName('tags_groups', $Project);
 
         // get ids
         $result = $DataBase->fetch([
@@ -47,6 +48,7 @@ class Cron
 
         $list = [];
         $_tmp = [];
+        QUI\System\Log::writeRecursive('Start Tags Tmp Things');
 
         foreach ($result as $entry) {
             $tags = \explode(',', $entry['tags']);
@@ -61,7 +63,7 @@ class Cron
 
                 $entry['id'] = (int)$entry['id'];
 
-                $_str = $entry['id'].'_'.$tag;
+                $_str = $entry['id'] . '_' . $tag;
 
 
                 if (isset($_tmp[$_str])) {
@@ -69,14 +71,17 @@ class Cron
                 }
 
                 $list[$tag][] = $entry['id'];
-                $_tmp[$_str]  = 1; // temp zum prüfen ob schon drinnen, in_array ist zulangsam
+                $_tmp[$_str] = 1; // temp zum prüfen ob schon drinnen, in_array ist zulangsam
             }
         }
+        QUI\System\Log::writeRecursive('End Tags Tmp Things');
+
 
         /**
          * Tag cache
          */
         $DataBase->table()->truncate($tableCache);
+        QUI\System\Log::writeRecursive('Start Tag cache');
 
         foreach ($list as $tag => $entry) {
             $siteIds = [];
@@ -96,15 +101,17 @@ class Cron
 
             $DataBase->insert($tableCache, [
                 'tag'   => $tag,
-                'sites' => ','.\implode(',', $siteIds).',',
+                'sites' => ',' . \implode(',', $siteIds) . ',',
                 'count' => \count($siteIds)
             ]);
         }
+        QUI\System\Log::writeRecursive('End Tag cache');
 
         /**
          * Sites cache
          */
         $DataBase->table()->truncate($tableSiteCache);
+        QUI\System\Log::writeRecursive('Start Sites cache');
 
         foreach ($result as $entry) {
             if (empty($entry['tags'])) {
@@ -130,11 +137,53 @@ class Cron
                     continue;
                 }
 
+                /** die Gruppen müssen evaluiert werden
+                 * für jeden Tag muss nach der Gruppe geschaut werden und in eine Liste geschrieben
+                 */
+                $groupsIds = [];
+                $groupsIdsAssoc = [];
+                $tags = \explode(',', $entry['tags']);
+                foreach ($tags as $tag) {
+                    if (empty($tag)) {
+                        continue;
+                    }
+
+                    $query = "SELECT * FROM $tableTagGroups WHERE tags LIKE '%,$tag,%'";
+                    $result = $DataBase->fetchSQL($query);
+
+                    /**
+                     * for every group add id to the List
+                     */
+                    $currentGroup = [];
+                    foreach ($result as $foundGroup) {
+                        $groupId = $foundGroup['id'];
+                        $groupsIds[] = $groupId;
+                        $currentGroup[] = $groupId;
+                    }
+                    if (!isEmpty($currentGroup)) {
+                        $groupsIdsAssoc[$tag] = $currentGroup;
+                    }
+                }
+
+                $goupsStr = ',' . \implode(',', $groupsIds) . ',';
+
+                if (!isEmpty($groupsIds)) {
+                    QUI\System\Log::writeRecursive([
+                        'siteId' => $entry['id'],
+                        'tags' => $entry['tags'],
+//                        'groups' => $groupsIds,
+                        '$goupsStr' => $goupsStr,
+//                        '$groupsIdsAssoc' => $groupsIdsAssoc
+                    ]);
+                }
+
+
                 $DataBase->insert($tableSiteCache, [
                     'id'     => $Site->getId(),
                     'name'   => $Site->getAttribute('name'),
                     'title'  => $Site->getAttribute('title'),
                     'tags'   => $entry['tags'],
+                    'groups'   => $goupsStr,
                     'c_date' => $Site->getAttribute('c_date'),
                     'e_date' => $Site->getAttribute('e_date')
                 ]);
